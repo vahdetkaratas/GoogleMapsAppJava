79c919ea7e1ea72a8c41db6930aca24a9d6ef337
Vahdet Karatas <38797434+vahdetkaratas@users.noreply.github.com>
2023-10-23 19:06:51 +0300

    first commit
    Working with GoogleMAPS

---
+++ app/src/main/java/com/karatas/googlemapsappjava/roomdb/PlaceDatabase.java
@@ -0,0 +1,12 @@
+package com.karatas.googlemapsappjava.roomdb;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.karatas.googlemapsappjava.model.Place;
+
+
+@Database(entities = {Place.class}, version = 1)
+public abstract class PlaceDatabase extends RoomDatabase {
+    public abstract PlaceDao placeDao();
+}
